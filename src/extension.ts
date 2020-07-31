fb5a06e064dfb7c1aea5a12f9c1d34b58b32c01f
courvoisier <courvoisier.christophe@gmail.com>
2020-07-31 10:06:22 +0200

    rename extension & cmd

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "al-object-range" is now active!');
+	console.log('Congratulations, your extension "al-extension-description" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('al-object-range.generate-object-range-report', () => {
+	let disposable = vscode.commands.registerCommand('al-extension-description.generate-extension-report', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
